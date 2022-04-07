4e2353e3c7fe49074dd8b53f22a6085bf0a55c5e
anna <a.sahradyan72@gmail.com>
2022-04-07 15:48:50 +0400

    changed component movieDetails

--- src/components/BookDetails/index.js
+++ src/components/BookDetails/index.js
@@ -2,7 +2,6 @@ import React, {useEffect} from 'react';
 import s from './BookDetails.module.scss';
 import {useDispatch, useSelector} from "react-redux";
 import {useParams} from "react-router-dom";
-import BooksPage from "../BooksPage";
 import {fetchAsyncAllBooksId, selectOneBook} from "../../store/detailSlice";
 
 
@@ -24,18 +23,9 @@ const BookDetails = () => {
             <div className={s.contentPage}>
                 <div className={s.containerPage}>
                     <div className={s.cardList}>
-                        {allBooks.kind === 'books#volumes' ? (books.map((item, index) => {
-                            return <BooksPage key={`${item}_${index}`} title={item.volumeInfo['title']}
-                                              booksId={item.id}
-                                              img={item.volumeInfo.imageLinks['thumbnail']}
-                                              authors={item.volumeInfo['authors']}
-                                              description={item.volumeInfo['description']}
-                                              categories={item.volumeInfo['categories']}
-                                              language={item.volumeInfo['language']}
-                                              publishedDate={item.volumeInfo['publishedDate']}/>
-                        })) : (<div><h1>{allBooks.Error}</h1></div>)}
 
-                    </div>
+
+                     </div>
                 </div>
 
 
